d714cbe7ddd52e7b0cfe4513789f8b325d3c8189
unknown <amberkayeafr6901@gmail.comcls>
2023-05-23 19:34:17 +0200

    urhm

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-//variables stuffies
+function calc(){
+      let bmi;
 
+      let result = document.getElementById("bmi");
 
-let weightbtn = document.querySelector(".weight");
-let heightbtn = document.querySelector(".height");
-let submitBtn = document.querySelector(".button");
-let clearBtn = document.querySelector(".allClear");
-let bMi = document.querySelector(".text");
-let text = document.querySelector(".massp");
-
-let BMI, weight, height;
-//clickevent for btn
-function calc() {
-    ("click",() =>{
-    weight = weightbtn.value;
-    height = heightbtn.value;
-    BMI = (weight/height*height);
-    bMi.innerHTML = BMI;
-    if(BMI < 18.5){
-        text.innerText = "You are an underweight bestie";
-    }else if((BMI > 18.5) && (BMI <= 24.9)){
-        text.innerText = "Bestie, You are a Normie";
-    }else if((BMI >= 25) && (BMI <= 29.9 )){
-        text.innerText = "You are a chubby bestie";
-    }else{
-        text.innerText = "You enjoy sweeties too much, besties";
+      let height = parseInt(document.getElementById("height").value);
+      let weight = parseInt(document.getElementById("weight").value);
+  
+      let text = document.getElementById("text"),
+      bmI = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+      result.innerHTML = bmi;
+  
+  
+      if(bmi < 18.5){
+          result.innerHTML = bmI;
+          text.innerHTML = 'underweight';
+      }
+  
+      else if( bmI >= 18.5 && bmI <= 24.9 ){
+          result.innerHTML = bmI;
+          text.innerHTML = 'healthy';
+      }
+  
+    
+      else if( bmI >= 25 && bmI <= 29.9 ){
+          result.innerHTML = bmI;
+          text.innerHTML = 'overweight';
+      }
+  
+      else{
+         result.innerHTML = bmI;
+         text.innerHTML = 'obese';
+      }
     }
-})
-
-}
-//new function
+    //nother function
+    function clearC() {
+        document.getElementById("result").value = "";
+        document.getElementById("height").value = "";
+        document.getElementById("weight").value = "";
+        document.getElementById("text").value = "";
+    }
